886fc7bc334ff593a75ab4501d316c3d4b3b8964
KostaIvanovski <kostaivanovski.official@gmail.com>
2022-05-27 10:22:01 +0200

    placeholder for display and calc functions

--- main.js
+++ main.js
@@ -4,13 +4,79 @@ let displayBot = document.querySelector("#displayBot");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.id);
-    displayInput(e);
+    let id = e.target.id;
+    displayInput(id);
+    if (id === "delete") erase(displayBot);
+    if (id === "equals") calculateInput(id);
   });
 });
 
-function displayInput(e) {
-  let id = e.target.id;
+let firstNumber;
+let operationType;
+let isOperationActive = false;
+let secondNumber;
+let rezult;
+
+function calculateInput(id) {
+  if (id === "one") {
+    if (!isOperationActive) firstNumber += "1";
+    if (isOperationActive) secondNumber += "1";
+  }
+  if (id === "two") {
+    if (!isOperationActive) firstNumber += "2";
+    if (isOperationActive) secondNumber += "2";
+  }
+  if (id === "three") {
+    if (!isOperationActive) firstNumber += "3";
+    if (isOperationActive) secondNumber += "3";
+  }
+  if (id === "four") {
+    if (!isOperationActive) firstNumber += "4";
+    if (isOperationActive) secondNumber += "4";
+  }
+  if (id === "five") {
+    if (!isOperationActive) firstNumber += "5";
+    if (isOperationActive) secondNumber += "5";
+  }
+  if (id === "six") {
+    if (!isOperationActive) firstNumber += "6";
+    if (isOperationActive) secondNumber += "6";
+  }
+  if (id === "seven") {
+    if (!isOperationActive) firstNumber += "7";
+    if (isOperationActive) secondNumber += "7";
+  }
+  if (id === "eight") {
+    if (!isOperationActive) firstNumber += "8";
+    if (isOperationActive) secondNumber += "8";
+  }
+  if (id === "nine") {
+    if (!isOperationActive) firstNumber += "9";
+    if (isOperationActive) secondNumber += "9";
+  }
+  if (id === "0") {
+    if (!isOperationActive) firstNumber += "0";
+    if (isOperationActive) secondNumber += "0";
+  }
+  if (id === "plus") {
+    operationType = "add";
+    isOperationActive = true;
+  }
+  if (id === "minus") {
+    operationType = "subtract";
+    isOperationActive = true;
+  }
+  if (id === "star") {
+    operationType = "multiply";
+    isOperationActive = true;
+  }
+  if (id === "backtick") {
+    operationType = "divide";
+    isOperationActive = true;
+  }
+}
+
+function displayInput(id) {
   switch (id) {
     case "one":
       displayBot.innerText += "1";
@@ -51,14 +117,11 @@ function displayInput(e) {
     case "star":
       displayBot.innerText += "*";
       break;
-    case "dot":
-      displayBot.innerText += ".";
-      break;
     case "backtick":
       displayBot.innerText += "/";
       break;
-    case "equals":
-      displayBot.innerText += "=";
+    case "dot":
+      displayBot.innerText += ".";
       break;
 
     default:
@@ -69,14 +132,28 @@ function displayInput(e) {
 function add(num1, num2) {
   return num1 + num2;
 }
+
 function subtract(num1, num2) {
   return num1 - num2;
 }
+
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
 function divide(num1, num2) {
   return num1 / num2;
 }
-function clear() {}
-function erase() {}
+
+function clear() {
+  displayTop.innerText = "";
+  displayBot.innerText = "";
+}
+
+function erase(displayElement) {
+  displayElement.innerText = displayElement.innerText.slice(0, displayElement.innerText.length - 1);
+  firstNumber = null;
+  secondNumber = null;
+  operation = null;
+  rezult = null;
+}
